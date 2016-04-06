18c3d77145c5839416969cfafc771949a0342f57
Matias Aiskovich <matias.aiskovich@caretronics.com.ar>
2016-04-06 02:04:58 -0300

    Changes cdn with ssl problems to local files.
    Changes build process to include local leaflet files.

--- gulp/build.js
+++ gulp/build.js
@@ -90,9 +90,20 @@ gulp.task('other', function () {
     .pipe(fileFilter)
     .pipe(gulp.dest(path.join(conf.paths.dist, '/')));
 });
+gulp.task('leaflet', function () {
+  var fileFilter = $.filter(function (file) {
+    return file.stat.isFile();
+  });
+
+  return gulp.src([
+    path.join(conf.paths.src, '/assets/leaflet/*')
+  ])
+    .pipe(fileFilter)
+    .pipe(gulp.dest(path.join(conf.paths.dist, '/assets/leaflet/')));
+});
 
 gulp.task('clean', function () {
   return $.del([path.join(conf.paths.dist, '/'), path.join(conf.paths.tmp, '/')]);
 });
 
-gulp.task('build', ['html', 'fonts', 'other']);
+gulp.task('build', ['html', 'fonts', 'other', 'leaflet']);
